125260d80868876013b21ccee130215d3f270dd6
Pablo Fuenzalida <115206655+ecologistpablo@users.noreply.github.com>
2023-09-06 17:18:28 +1000

    monthly averages

--- 8_230906_extract_SST.R
+++ 8_230906_extract_SST.R
@@ -1,6 +1,6 @@
 # 04 September 2023
-# Who needs remora anyway
-# Interpolating NAs in SST data
+  # Who needs remora anyway
+    # Interpolating NAs in SST data
 
 rm(list=ls())
 setwd("~/University/2023/Honours/R/data/git/NC-wrestling")
@@ -32,20 +32,14 @@ plot(pts.UTM, add = T)
 
 # extract -----------------------------------------------------------------
 
-sst.pts <- extract(rstack, pts.UTM, ID = F) 
-# ID = FALSE otherwise it creates a column with a number for each spoint
+sst.pts <- extract(rstack, pts.UTM, ID = F) # ID = FALSE otherwise it creates a column with a number for each spoint
 
-str(sst.pts)
-
-head(sst.pts)
-
-hist(sst.pts[, 1], col="grey", main="Max temp (°C)") 
 
 sum(is.na(sst.pts))
 3871 * 127 #how many obs in total
-491617 - 349402 #total obs - NAs
-#total obs = 142,215 out of 491,167
-#appox. 75% is NA
+(349402 / 491617) * 100 
+
+#71% is NA
 
 # nearest temporal neighbour ----------------------------------------------
 
@@ -66,12 +60,10 @@ sst.pts1 <- t(apply(sst.pts, 1, function(row) { # Apply a function to each row o
 
 sst.pts1 <- as.data.frame(sst.pts1)
 
-sum(is.na(sst.pts)) - sum(is.na(sst.pts1)) 
-#we filled 132,590 points
+sum(is.na(sst.pts1)) #216812
 
-sum(is.na(sst.pts1))
-491617 - 216812
-#still have approx. 50% of NAs in the df
+(216812 / 491617) * 100 # 44.1% NA
+
 
 # 5 d mean ----------------------------------------------------------------
 
@@ -97,32 +89,26 @@ mean_5d <- function(row) {
 # Apply the function to each row and save the new values in sst.pts2
 sst.pts2 <- t(apply(sst.pts1, 1, mean_5d))
 
-#more munging
 sst.pts2 <- as.data.frame(sst.pts2)
 colnames(sst.pts2) <- colnames(sst.pts1)
 
-sum(is.na(sst.pts1)) - sum(is.na(sst.pts2)) 
-#only 22 were filled :o
+sum(is.na(sst.pts1)) - sum(is.na(sst.pts2))  #only 22 were filled :o
 
 sum(is.na(sst.pts2)) 
-#still 216,790 out of 491,000 that are NA
+(216790 / 491617) * 100 #44.09% are NA at a 2km resolution
 
+#with nearest neighbour single day interpolation & 5 d mean
 
 # increase coarseness -----------------------------------------------------
+
 rstack #res at 0.02 by 0.02, 2km x 2km
 
-rstack10km <- aggregate(rstack, fact = 5.5, fun=mean, na.rm = TRUE)
+rstack10km <- aggregate(rstack, fact = 5.5, #factor of whatever your resolution is in the OG raster / stack
+                        fun = mean, na.rm = TRUE)
 
 # resample ----------------------------------------------------------------
 
 sst.pts10km <- extract(rstack10km, pts.UTM, ID = F) 
-#manually inspect, will travelling in time help a lot?
-
-sum(is.na(sst.pts10km))
-sum(is.na(sst.pts5km))
-sum(is.na(sst.pts4km))
-
-#oke lets try our 10km buffer since it has the least amount of NAs
 
 # nearest temporal neighbour ----------------------------------------------
 
@@ -141,13 +127,10 @@ sst.pts10km1 <- t(apply(sst.pts10km, 1, function(row) { # Apply a function to ea
   return(row)  # Return the modified row.
 }))
 
-sst.pts1 <- as.data.frame(sst.pts1)
+sst.pts1 <- as.data.frame(sst.pts1) #convert back to df
 
-sum(is.na(sst.pts10km)) - sum(is.na(sst.pts10km1)) 
-#we filled 160500 points
+sum(is.na(sst.pts10km1)) #still got 81324 NAs
 
-sum(is.na(sst.pts10km1))
-#still got 81324 NAs
 
 # 5 d mean ----------------------------------------------------------------
 
@@ -177,11 +160,36 @@ fill_vals <- function(sst_pts2, sst_pts10km2) {
   return(sst_pts2)
 }
 
-# Example usage
+#fill values of 10km res into our 2km res
 sst.pts3 <- fill_vals(sst.pts2, sst.pts10km2)
 
-sum(is.na(sst.pts3)) - sum(is.na(sst.pts2))
+sum(is.na(sst.pts3))
 
-# %
 (81301 / 491617) * 100
 #16% of our data is still NA :(
+
+44 - 16
+#28 % is sampled at 10km spatial resolution
+
+# rm NA rows --------------------------------------------------------------
+
+#we plotted the spatial points of all rows that contain majority of NAs
+#They're in Syd and Jervis bay in corners, can we remove them ?!
+
+
+sst.pts3 <- sst.pts3 %>% mutate(RowNumber = row_number()) #make a row number 
+# Remove rows that have more than 10 NA values
+sst.pts4 <- sst.pts3[apply(sst.pts3, 1, function(x) sum(is.na(x)) <= 10), ]
+
+sum(is.na(sst.pts4)) #10 obs out of 491,000 is pretty good
+
+
+#to find out which rows we removed:
+NA1 <- sst.pts3 %>% mutate(RowNumber = row_number()) #make a row number 
+NA2 <- NA1[apply(NA1, 1, function(x) sum(is.na(x)) > 10), ] #bring only rows with + 10NAs 
+NA3 <- NA2[3872:3872] #only keep RowNumber column
+
+# save --------------------------------------------------------------------
+
+write_csv(sst.pts4, file = "Inputs/SST_vals_12-22_230906.csv")
+write_csv(NA3, file = "Inputs/NA_rows_12-22.csv")
